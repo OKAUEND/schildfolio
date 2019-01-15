916778f029977bd7af2b7905935a34e0cb8d1c95
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-01-16 04:58:05 +0900

    Revert "ユーザー登録機能の完成"
    This reverts commit 2ee58cf12568b211cc5d54a7890757ef5654998b.

--- bord/function.php
+++ bord/function.php
@@ -1,78 +1,29 @@
 <?php 
 
-//---------------------------------------------
-// ログ
-//---------------------------------------------
 //ログを取るか
 ini_set('log_errors','on');
-//ログの出力ファイルを指定
-ini_set('error_log','php.log');
 
-//---------------------------------------------
-// デバッグ設定
-//---------------------------------------------
-$debug_flg = true;
-function debug($str)
-{
-    global $debug_flg;
-    if(!empty($debug_flg))
-    {
-        error_log('デバッグ',$str);
-    }
-}
+//ログー出力設定
 
-//---------------------------------------------
-// 画面表示処理開始ログ吐き出し関数
-//---------------------------------------------
-function debuglogstart()
-{
-    debug('------------------------>画面表示処理開始');
-    debug('セッションID:'.session_id());
-    debug('セッション変数:' .print_r($_SESSION,true));
-    debug('現在日時タイムスタンプ:'.time());
-    if(!empty($_SESSION['login_date']) && !empty($_SESSION['login_limit']))
-    {
-        debug('ログイン期限日時タイムスタンプ:'.($_SESSION['login_date'] + $_SESSION['login_limit']));
-    }
-}
-
-//---------------------------------------------
-// 定数：注意/警告メッセージ
-//---------------------------------------------
-//テスト用メッセージ
-const TEST_MSG = "TEST:メッセージだよ:TEST";
-const TEST_MSG_ERROR ="TEST_ERROR:ユーザー名がありません。:TEST_ERROR";
-
-//本番用メッセージ
-const MSG_EMPTY = "入力必須項目です。";
-const MSG_EMAIL_ERROR = "Emailの形式で入力してください。";
-const MSG_EMAIL_DUP = "すでにEmailアドレスが登録されています。";
+const TEST_MSG = "テスト用メッセージだよ";
+const TEST_MSG_ERROR ="テスト用エラー:ユーザー名がありません。";
 const MSG_HALF_ERROR = "半角英数字で入力をしてください。";
-const MSG_MIN_LENGTH = "8文字以上で入力してください。";
-const MSG_MAX_LENGTH = "255文字以内で入力してください。";
 
-const MSG_CONECT_ERROR = "エラーが発生しました。時間をおいてから再度操作をしてください。";
 
-//---------------------------------------------
 //エラーメッセージ出力用配列
-//---------------------------------------------
 $err_msg = array();
 
-//---------------------------------------------
 //文字の入力チェック
-//---------------------------------------------
 function IsNullRequired($value,$key)
 {
     if(empty($value))
     {
         global $err_msg;
-        $err_msg[$key] = MSG_EMPTY ;
+        $err_msg[$key] = TEST_MSG_ERROR ;
     }
 }
 
-//---------------------------------------------
 //半角英数字チェック
-//---------------------------------------------
 function IsVaildHalfString($value,$key)
 {
     var_dump($value);
@@ -85,9 +36,6 @@ function IsVaildHalfString($value,$key)
     }
 }
 
-//---------------------------------------------
-//半角記号チェック
-//---------------------------------------------
 function IsVaildSymbol($value,$key)
 {
     if(!preg_match("/^[ -]+$/",$value))
@@ -96,74 +44,59 @@ function IsVaildSymbol($value,$key)
     }
 }
 
-//---------------------------------------------
 //Emailか文字列チェック
-//---------------------------------------------
 function IsVaildEmail($value,$key)
 {
-    //if(!preg_match("/\A\w+@\d+\.\d+\z/",$value))
-    if(!(bool)filter_var($value,FILTER_VALIDATE_EMAIL))
+    if(!preg_match("",$value))
     {
-        global $err_msg;
-        $err_msg[$key] = MSG_EMAIL_ERROR; 
+        
     }
 }
 
-//---------------------------------------------
 //文字列の最小チェック
-//---------------------------------------------
 function IsMinStrLen($value,$key,$minlen = 8)
 {
     var_dump(strlen($value));
     if(strlen($value) <= $minlen)
     {
         global $err_msg;
-        $err_msg[$key] = MSG_MIN_LENGTH; 
+        $err_msg[$key] = TEST_MSG_ERROR; 
     }
 }
 
-//---------------------------------------------
 //文字列の最大値チェック
-//---------------------------------------------
 function IsMaxStrLen($value,$key,$maxlen = 15)
 {
     if(strlen($value) >= $maxlen)
     {
         global $err_msg;
-        $err_msg[$key] = MSG_MAX_LENGTH; 
+        $err_msg[$key] = TEST_MSG_ERROR; 
     }
 }
 
-//---------------------------------------------
 //文字列がDBに存在してるかのチェック
-//---------------------------------------------
 function IsEmailDup($value,$key)
 {
     global $err_msg;
 
     try
     {
-        //DBの接続設定
         $dbh = DBConnect(); 
 
-        //クエリ文を生成
         $sql = 'SELECT count(*) as Email FROM users WHERE email = :email';
         $sql_date = array(':email' => $email);
 
-        //クエリを発行&取得
         $stmt = queryPost($dbh,$sql,$sql_date);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        //結果判定
-        //値があれば重複していると判定
         if(!empty(arrya_shift($result)))
         {
-            $err_msg[$key] = MSG_EMAIL_DUP;
+            $err_msg['email'] = "";
         }
     }
     catch(Exception $e)
     {
-        $err_msg['common'] = MSG_CONECT_ERROR;
+        $err_msg['common'] = 'SQLエラー発生';
     }
 }
 
@@ -173,7 +106,6 @@ function IsEmailDup($value,$key)
 //
 ///////////////////////////////////////////////////////////////////////////////////
 //DB接続設定関数
-//---------------------------------------------
 function DBConnect()
 {
     $dsn = 'mysql:dbname=bord;host=localhost;charset=utf8';
@@ -193,9 +125,7 @@ function DBConnect()
     return new PDO($dsn,$user,$password,$option);
 }
 
-//---------------------------------------------
 //クエリ発行用関数
-//---------------------------------------------
 function queryPost($dbh,$sql,$data)
 {
     $stmt = $dbh->prepare($sql);
